70ec607b627a66655dabb228feae2fe80a8c7bdf
Cristian Colorado <ccoloradoc@gmail.com>
2018-01-23 11:30:58 -0600

    first commit

---
+++ app/server.js
@@ -0,0 +1,58 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var sessionInitalizer = require('./config/session');
+var env       = process.env.NODE_ENV || 'development';
+
+var attendance = require('./routes/index');
+
+var app = express();
+
+// Initialize session management
+sessionInitalizer(app);
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use('/attendance', attendance);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    console.log(err.message)
+    res.render('error', {
+      title: `Error ${err.status}`,
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    title: `Error ${err.status}`,
+    message: err.message,
+    error: {}
+  });
+});
+
+module.exports = app;
