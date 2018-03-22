9ad20efa42c272a14955db9d9269b7acf5e54d6b
Cristian Colorado <cristian.colorado@hbr.org>
2018-03-22 15:36:27 -0600

    Beatrix: Updating cache service

--- app/routes/deputy.js
+++ app/routes/deputy.js
@@ -1,48 +1,27 @@
 var express = require('express');
 var router = express.Router();
 var models  = require('../models');
+var DeputyService = require('../services/deputy');
 
-/* Path: /legislature/LXIII/deputy => deputy.js
- /api/legislature/LXIII/deputy/<seatId>/attendance
- /api/legislature/LXIII/deputy/<seatId>/initiatives
+const deputyService = new DeputyService();
+
+/* Path: /camara-de-diputados/LXIII/deputy => deputy.js
+ /api/camara-de-diputados/LXIII/deputy/<seatId>/attendance
+ /api/camara-de-diputados/LXIII/deputy/<seatId>/initiatives
 */
 
-/* /api/legislature/LXIII/deputy/<seatId>/attendance */
+/* /api/camara-de-diputados/LXIII/deputy/<seatId>/attendance */
 router.get('/:id/attendance', function(req, res, next) {
-  let queryString =
-    'select a.attendance as name, a.description, count(1) as value from Attendances a left outer join Deputies de on de.id = a.DeputyId left outer join Seats s on s.id = de.SeatId where s.id = :districtId group by a.attendance, a.description';
-
-  models.sequelize
-  .query(queryString, {
-    replacements: { districtId: req.params.id },
-    type: models.sequelize.QueryTypes.SELECT
-  })
-  .then(function(attendance) {
-    // Calculate positive attendance days
-    let total = attendance.reduce((acc, item) => {
-      switch(item.name) {
-        case 'A':
-        case 'AO':
-        case 'PM':
-        case 'IV':
-          return acc + item.value;
-        default:
-          return acc;
-      }
-    }, 0);
-
-    res.json({
-      details: attendance,
-      total: total
-    });
+  deputyService.getAttendanceDetails(req.params.id).then((result) => {
+    res.json(result);
   });
 });
 
 module.exports = router;
 
-/* Path: /legislature/LXIII/initiatives => initiatives.js
-  /api/legislature/LXIII/initiatives/avg
-  /api/legislature/LXIII/initiatives/frequency
-  /api/legislature/LXIII/initiatives/by_party
-  /api/legislature/LXIII/initiatives/by_deputy_type
+/* Path: /camara-de-diputados/LXIII/initiatives => initiatives.js
+  /api/camara-de-diputados/LXIII/initiatives/avg
+  /api/camara-de-diputados/LXIII/initiatives/frequency
+  /api/camara-de-diputados/LXIII/initiatives/by_party
+  /api/camara-de-diputados/LXIII/initiatives/by_deputy_type
 */
